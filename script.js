b1fa28d734875ab2ecb7f285ad522c478b8f6786
addlinakinyi-coder <addlinakinyi@gmail.com>
2026-04-13 00:33:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-body { font-family: sans-serif; margin: 0; background: #f0f2f5; color: #333; }
-nav { 
-    display: flex; justify-content: space-between; 
-    padding: 15px; background: #222; color: white; align-items: center;
+const myKey = "390f977d921d42660f03c1ef22c0a36e";
+const myCity = "Nairobi"; 
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${myCity}&appid=${myKey}&units=metric`;
+
+async function getLocalWeather() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        if (data.cod === 200) {
+            document.getElementById('city').innerText = data.name;
+            document.getElementById('temp').innerText = ` | ${Math.round(data.main.temp)}°C`;
+        } else {
+            document.getElementById('city').innerText = "Updating...";
+        }
+    } catch (err) {
+        console.error("Weather error:", err);
+    }
 }
-#weather-box { background: #444; padding: 8px; border-radius: 5px; font-size: 0.8rem; }
-.hero { text-align: center; padding: 60px 20px; background: white; }
-.grid { display: grid; grid-template-columns: 1fr; gap: 20px; padding: 20px; }
-.card { background: white; padding: 30px; border-radius: 10px; box-shadow: 0 4px 6px rgba(0,0,0,0.1); text-align: center; font-weight: bold; }
-@media (min-width: 600px) { .grid { grid-template-columns: 1fr 1fr 1fr; } }
+
+getLocalWeather();
